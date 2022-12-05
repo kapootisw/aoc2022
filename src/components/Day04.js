0edc5e276accaccd3aa33f034d1cf82f16db8036
lewis.walther <lewis.walther@rainfocus.com>
2022-12-05 13:46:52 -0700

    Finished Day 4 part 2

--- src/components/Day04.js
+++ src/components/Day04.js
@@ -3,7 +3,6 @@ import { Day } from './Day'
 export const Day04 = () => {
   const calculatePart1 = (input) => {
     const lines = input.split('\n')
-    console.log(lines)
     const fullyContains = lines.filter(line => {
       if (line === '') {
         return false
@@ -34,10 +33,43 @@ export const Day04 = () => {
     return fullyContains.length
   }
 
+  const calculatePart2 = (input) => {
+    const lines = input.split('\n')
+    const overlaps = lines.filter(line => {
+      if (line === '') {
+        return false
+      }
+
+      const assignments = line.split(',')
+      const elf1 = assignments[0].split('-')
+      const elf2 = assignments[1].split('-')
+
+      const elf1Start = Number(elf1[0])
+      const elf1End = Number(elf1[1])
+      const elf2Start = Number(elf2[0])
+      const elf2End = Number(elf2[1])
+
+      const elf1OverlapsElf2Start = elf1Start <= elf2Start && elf1End >= elf2Start
+      const elf2OverlapsElf1Start = elf2Start <= elf1Start && elf2End >= elf1Start
+
+      const elf1OverlapsElf2End = elf1End >= elf2End && elf1Start <= elf2End
+      const elf2OverlapsElf1End = elf2End >= elf1End && elf2Start <= elf1End
+
+      const elf1OverlapsElf2 = elf1OverlapsElf2Start || elf1OverlapsElf2End
+      const elf2OverlapsElf1 = elf2OverlapsElf1Start || elf2OverlapsElf1End
+
+      return elf1OverlapsElf2 || elf2OverlapsElf1
+    })
+    console.log(overlaps)
+
+    return overlaps.length
+  }
+
   return (
     <Day
       label={"Day 04"}
       calculatePart1={calculatePart1}
+      calculatePart2={calculatePart2}
     />
   )
 }
