e3e72dce486dd8c034aace2de0b10a725517f05f
lewis.walther <lewis.walther@rainfocus.com>
2022-12-08 17:05:17 -0700

    Finished Day 6 part 1 and 2

--- src/components/Day06.js
+++ src/components/Day06.js
@@ -2,12 +2,23 @@ import { Day } from './Day'
 
 export const Day06 = () => {
   const calculatePart1 = (input) => {
-    console.log(input)
     let startOfPacket = -1
     for (let i = 4; i < input.length; i++) {
       const lastFourChars = input.substring(i - 4, i)
       const hasMatchingChars = lastFourChars.split('').some((char, index) => lastFourChars.lastIndexOf(char) !== index)
-      console.log(lastFourChars, hasMatchingChars)
+      if (!hasMatchingChars) {
+        startOfPacket = i
+        break
+      }
+    }
+    return startOfPacket
+  }
+
+  const calculatePart2 = (input) => {
+    let startOfPacket = -1
+    for (let i = 14; i < input.length; i++) {
+      const lastFourteenChars = input.substring(i - 14, i)
+      const hasMatchingChars = lastFourteenChars.split('').some((char, index) => lastFourteenChars.lastIndexOf(char) !== index)
       if (!hasMatchingChars) {
         startOfPacket = i
         break
@@ -20,6 +31,7 @@ export const Day06 = () => {
     <Day
       label={"Day 06"}
       calculatePart1={calculatePart1}
+      calculatePart2={calculatePart2}
     />
   )
 }
